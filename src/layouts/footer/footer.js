42d1e694616c42f37f40c3d1a92a54553ed10548
Toni <tonig@catalyst.net.nz>
2016-09-26 11:55:08 +1300

    RM34662 footer front end styling

--- src/layouts/footer/footer.js
+++ src/layouts/footer/footer.js
@@ -7,10 +7,18 @@ class Footer extends Component {
   render () {
     return (
       <footer className='page-footer'>
-        <div className='page-footer-inner'>
-          <Link to={'/contact-us/'}>Contact us</Link>
-          <Link to={'/your-privacy/'}>Your privacy</Link>
-          <Link to={'/copyright-and-attribution/'}>Copyright and attribution</Link>
+        <span className='page-footer-curve'></span>
+        <div className='page-footer-inner-wrap'>
+          <div className='page-footer-inner'>
+            <ul>
+              <li><Link to={'/contact-us/'}>Contact us</Link></li>
+              <li><Link to={'/your-privacy/'}>Your privacy</Link></li>
+              <li><Link to={'/copyright-and-attribution/'}>Copyright and attribution</Link></li>
+            </ul>
+            <p className='nz-govt'>
+              <a href='https://www.govt.nz/'><img src='/assets/img/nz-govt-logo-black.svg' alt='New Zealand Government' /></a>
+            </p>
+          </div>
         </div>
       </footer>
     )
