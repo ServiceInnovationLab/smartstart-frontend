dd67cbe58b7655531f0b12b1e60500ee05b96759
Jen Zajac <jen@catalyst.net.nz>
2016-10-03 17:10:35 +1300

    RM37540 Add phase titles
    Still need to make phase titles link to phases

--- src/components/settings-pane/todo-list/todo-list.js
+++ src/components/settings-pane/todo-list/todo-list.js
@@ -10,24 +10,34 @@ class TodoList extends Component {
       'settings-pane',
       { 'is-open': this.props.shown }
     )
+    let phaseTasks = []
+
+    this.props.phases.forEach(phase => {
+      let tasks = []
+
+      if (phase.elements) {
+        phase.elements.forEach(card => {
+          if (card.elements) {
+            card.elements.forEach(element => {
+              if (element.tags.indexOf('boac_presentation::task') >= 0) {
+                tasks.push(<Task key={element.id} id={element.id} type={element.type} label={element.label} text={element.content} />)
+              }
+            })
+          }
+        })
+
+        if (tasks.length) {
+          phaseTasks.push(<h5 key={'todo-phase-' + phase.id}>{phase.label}</h5>)
+          phaseTasks.push(tasks)
+        }
+      }
+    })
 
     return (
       <div id='todo-list' className={paneClasses} aria-hidden={!this.props.shown}>
         <h4>To do list</h4>
 
-        {this.props.phases.map(phase => {
-          if (!phase.elements) { phase.elements = [] } // a phase can be empty
-
-          return phase.elements.map(card => {
-            if (!card.elements) { card.elements = [] } // a card can be empty)
-
-            return card.elements.map(element => {
-              if (element.tags.indexOf('boac_presentation::task') >= 0) {
-                return <Task key={element.id} id={element.id} type={element.type} label={element.label} text={element.content} />
-              }
-            })
-          })
-        })}
+        {phaseTasks}
 
         <div className='button-set'>
           <button type='button' className='cancel-button' onClick={this.props.todoPaneClose}>Close</button>
