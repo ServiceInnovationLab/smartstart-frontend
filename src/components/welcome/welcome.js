053cd3f8ddc1e51907e2caac822300f7c47564e9
Jen Zajac <jen@catalyst.net.nz>
2016-10-05 14:48:44 +1300

    RM36675 Stub for welcome

---
+++ src/components/welcome/welcome.js
@@ -0,0 +1,13 @@
+import './welcome.scss'
+
+import React, { Component } from 'react'
+
+class Welcome extends Component {
+  render () {
+    return (
+      <h2>Welcome to SmartStart</h2>
+    )
+  }
+}
+
+export default Welcome
