d51fb0ca78096c7c1ad1a136dec7a68e8bcc59ec
Jen Zajac <jen@catalyst.net.nz>
2016-10-13 15:14:25 +1300

    RM36446 update secondary login area

--- src/components/login-button/login-button.js
+++ src/components/login-button/login-button.js
@@ -7,9 +7,9 @@ import classNames from 'classnames'
 class LoginButton extends Component {
   render () {
     return (
-      <div className='realme_widget realme_secondary_login realme_theme_dark' data-test='login'>
-        <a href='/login/' className='realme_login realme_pipe ext-link-icon'>Login</a>
-        <a href='/login/' className='realme_create_account realme_pipe ext-link-icon'>Create</a>
+      <div className='realme-secondary-widget' data-test='login'>
+        <a href='/login/' className='ext-link-icon'>Login</a>
+        <a href='/login/' className='ext-link-icon'>Create</a>
       </div>
     )
   }
