3fbbb91f1522f196e0bc16d9400e635667ece688
Jen Zajac <jen@catalyst.net.nz>
2016-10-13 18:23:39 +1300

    RM36663 different messages for different states

--- src/components/messages/messages.js
+++ src/components/messages/messages.js
@@ -10,7 +10,9 @@ class Messages extends Component {
     super(props)
 
     this.state = {
-      loginMessageShown: false
+      initialLoginMessageShown: true,
+      insistentLoginMessageShown: false,
+      loggedInMessageShown: false
     }
 
     this.checkIfRealMeLoginShouldBeShown = this.checkIfRealMeLoginShouldBeShown.bind(this)
@@ -25,31 +27,65 @@ class Messages extends Component {
   }
 
   checkIfRealMeLoginShouldBeShown (isLoggedIn, personalisationValues) {
-    // to know if we should show the message, we just need to know if a) user is logged in and b) if there is any state
-    if (!isLoggedIn && Object.getOwnPropertyNames(personalisationValues).length > 0) {
+    // to know which message to show, we just need to know if a) user is logged in and b) if there is any state
+    if (isLoggedIn) {
       this.state = {
-        loginMessageShown: true
+        initialLoginMessageShown: false,
+        insistentLoginMessageShown: false,
+        loggedInMessageShown: true
+      }
+    } else if (!isLoggedIn && Object.getOwnPropertyNames(personalisationValues).length > 0) {
+      this.state = {
+        initialLoginMessageShown: false,
+        insistentLoginMessageShown: true,
+        loggedInMessageShown: false
       }
     } else {
       this.state = {
-        loginMessageShown: false
+        initialLoginMessageShown: true,
+        insistentLoginMessageShown: false,
+        loggedInMessageShown: false
       }
     }
   }
 
   render () {
-    let loginMessageClasses = classNames(
+    let initialLoginMessageClasses = classNames(
       'message',
-      { 'hidden': !this.state.loginMessageShown }
+      { 'hidden': !this.state.initialLoginMessageShown }
+    )
+    let insistentLoginMessageClasses = classNames(
+      'message',
+      { 'hidden': !this.state.insistentLoginMessageShown }
+    )
+    let loggedInMessageClasses = classNames(
+      'message',
+      { 'hidden': !this.state.loggedInMessageShown }
     )
 
     return (
       <div className='messages'>
-        <div className={loginMessageClasses}>
-          <h6>To save your profile for your next visit you need to log in with RealMe.</h6>
+        <p className={initialLoginMessageClasses}>
+          <a href='/login/'>Login with RealMe</a> to access and save your SmartStart profile and To Do list
+        </p>
+
+        <div className={insistentLoginMessageClasses}>
+          <p>To save your changes for your next visit you need to log in with RealMe.</p>
           <a className='button realme-primary-login-button ext-link-icon' href='/login/'>
             Login
           </a>
+          <div>
+            <p className='concertina'>What is RealMe?</p>
+            <div className='concertina-content'>
+              <p>RealMe is a New Zealand government service that lets you use one username and password to access a wide range of services online. To find out more go to www.RealMe.govt.nz</p>
+              <p>SmartStart uses RealMe to save and protect your information.</p>
+            </div>
+          </div>
+        </div>
+
+        <div className={loggedInMessageClasses}>
+          <h6>You are now logged in.</h6>
+          <p>Your changes will be saved. You will be logged out automatically after 30 minutes.</p>
         </div>
       </div>
     )
