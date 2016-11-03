c7b6d398e8281da03733b5b1654cfbe4854b4120
Jen Zajac <jen@catalyst.net.nz>
2016-11-03 17:52:58 +1300

    RM34665 track login and page loads

--- src/components/login-button/login-button.js
+++ src/components/login-button/login-button.js
@@ -1,13 +1,38 @@
 import './login-button.scss'
 
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { piwikTrackPost } from 'actions/actions'
 
 class LoginButton extends Component {
+  constructor (props) {
+    super(props)
+
+    this.loginAction = this.loginAction.bind(this)
+  }
+
+  loginAction (event) {
+    event.preventDefault()
+    const piwikEvent = {
+      'category': 'Login',
+      'action': 'Click',
+      'name': 'Secondary login'
+    }
+
+    // track the event
+    this.props.dispatch(piwikTrackPost('Secondary login', piwikEvent))
+
+    // match standard piwik outlink delay
+    window.setTimeout(() => {
+      window.location = '/login/'
+    }, 200)
+  }
+
   render () {
     return (
       <div className='realme-secondary-widget' data-test='login'>
-        <a href='/login/' className='ext-link-icon'>Login</a>
-        <a href='/login/' className='ext-link-icon'>Create</a>
+        <a href='/login/' onClick={this.loginAction} className='ext-link-icon'>Login</a>
+        <a href='/login/' onClick={this.loginAction} className='ext-link-icon'>Create</a>
       </div>
     )
   }
@@ -15,4 +40,8 @@ class LoginButton extends Component {
 
 LoginButton.propTypes = {}
 
-export default LoginButton
+function mapStateToProps (state) {
+  return {}
+}
+
+export default connect(mapStateToProps)(LoginButton)
