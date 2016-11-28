e30284eccbc49c2597e9ebf41041bc16cc1fce89
Jen Zajac <jen@catalyst.net.nz>
2016-11-28 14:18:54 +1100

    RM40828 added register birth welcome action

--- src/components/welcome/welcome.js
+++ src/components/welcome/welcome.js
@@ -12,6 +12,7 @@ class Welcome extends Component {
 
     this.profileClick = this.profileClick.bind(this)
     this.todoClick = this.todoClick.bind(this)
+    this.registerClick = this.registerClick.bind(this)
   }
 
   profileClick (event) {
@@ -42,6 +43,16 @@ class Welcome extends Component {
     this.props.dispatch(piwikTrackPost('Welcome', piwikEvent))
   }
 
+  registerClick () {
+    let piwikEvent = {
+      'category': 'Welcome',
+      'action': 'Click button',
+      'name': 'Register birth online'
+    }
+    // track the event
+    this.props.dispatch(piwikTrackPost('Welcome', piwikEvent))
+  }
+
   render () {
     return (
       <div className='welcome' role='banner'>
@@ -52,10 +63,11 @@ class Welcome extends Component {
             <p>SmartStart provides step-by-step information and support to help you access the right services for you and your baby.</p>
           </div>
 
-          <div role='navigation'>
+          <div className='welcome-content' role='navigation'>
             <a href='#' onClick={this.profileClick} aria-controls='my-profile' role='button' className='welcome-action welcome-action-personalise'>Add your due date</a>
-            <a href='#' onClick={this.todoClick} aria-controls='todo-list' role='button' className='welcome-action welcome-action-todo-list'>Check your To Do list</a>
             <JumpNav />
+            <a href='#' onClick={this.todoClick} aria-controls='todo-list' role='button' className='welcome-action welcome-action-todo-list'>Check your To Do list</a>
+            <a href='#161' onClick={this.registerClick} role='button' className='welcome-action welcome-action-register-birth'>Register birth online</a>
 
             <Messages />
           </div>
