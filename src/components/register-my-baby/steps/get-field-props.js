9af030030b979f1901fa75c688d11d165be2fa4f
Anh Trinh <anh.trinhtrung@gmail.com>
2017-08-28 11:43:10 +1200

    RM47890 fix pipeline failure because of wrong behavior happen on FireFox & IE when clicking the button while focus on text field

--- src/components/register-my-baby/steps/get-field-props.js
+++ src/components/register-my-baby/steps/get-field-props.js
@@ -1,6 +1,20 @@
 import omit from 'lodash/omit'
 
-const getFieldReviewProps = (schema, fieldName) => omit(schema[fieldName], ['validate'])
+export const preventBlurOnButtonClick = (e) => {
+  const { relatedTarget } = e
+  const type = relatedTarget ? relatedTarget.getAttribute('type') : ''
 
-export default getFieldReviewProps
+  if (type === 'button' || type === 'submit') {
+    e.preventDefault()
+  }
+}
+
+const getFieldProps = (schema, fieldName) => (
+  {
+    onBlur: preventBlurOnButtonClick,
+    ...omit(schema[fieldName], ['validate'])
+  }
+)
+
+export default getFieldProps
 
