515ea120cbac3c2df6710ba187f2c95a6ce7c220
Jen Zajac <jen@catalyst.net.nz>
2017-11-28 18:38:40 +1300

    LBS: first set of queries

--- src/actions/services.js
+++ src/actions/services.js
@@ -7,51 +7,6 @@ export const RECEIVE_SERVICES_API = 'RECEIVE_SERVICES_API'
 // TODO make sure this is passed in from config?
 const SERVICES_ENDPOINT = 'https://catalogue.data.govt.nz/api/action/datastore_search_sql?sql='
 
-// using select distinct (and not trying to return catergory data) means that
-// duplicate entries (across returned values) will be dropped
-
-let query = encodeURI(`SELECT DISTINCT
-  "PROVIDER_NAME",
-  "ORGANISATION_PURPOSE",
-  "SERVICE_NAME",
-  "SERVICE_DETAIL",
-  "PHYSICAL_ADDRESS",
-  "LATITUDE",
-  "LONGITUDE",
-  "PROVIDER_WEBSITE_1",
-  "PUBLISHED_CONTACT_EMAIL_1",
-  "PUBLISHED_PHONE_1",
-  "PROVIDER_CONTACT_AVAILABILITY"
-from "35de6bf8-b254-4025-89f5-da9eb6adf9a0"
-  WHERE
-    "LATITUDE" IS NOT NULL
-  AND
-    "LONGITUDE" IS NOT NULL
-  AND
-    "LATITUDE" != '0'
-  AND
-    "LONGITUDE" != '0'
-  AND
-  (
-      LOWER("SERVICE_DETAIL") LIKE '%breast%feeding%'
-    OR
-      LOWER("ORGANISATION_PURPOSE") LIKE '%breast%feeding%'
-    OR
-      "LEVEL_2_CATEGORY" LIKE '%Breast Feeding Support%'
-  )
-  ORDER BY
-    "LONGITUDE"`)
-
-  // (
-  //     "LEVEL_2_CATEGORY" LIKE '%Pregnancy%'
-  //   OR
-  //     "LEVEL_2_CATEGORY" LIKE '%Babies%'
-  //   OR
-  //     "LEVEL_2_CATEGORY" LIKE '%Young Parents%'
-  //   OR
-  //     "LEVEL_2_CATEGORY" LIKE '%Breast Feeding Support%'
-  // )
-
 // Action types
 
 function requestServicesAPI () {
@@ -69,7 +24,7 @@ function receiveServicesAPI (json) {
 
 // Action creators
 
-export function fetchServicesDirectory () {
+export function fetchServicesDirectory (query) {
   return dispatch => {
     dispatch(requestServicesAPI())
     return fetch(SERVICES_ENDPOINT + query) // TODO set in webpack config
