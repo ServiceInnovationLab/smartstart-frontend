086b094587f1f4d3c9f8935eabeccfde4854b9a5
Jen Zajac <jen@catalyst.net.nz>
2017-12-13 15:32:05 +1300

    LBS: click toggle map markers

--- src/components/services/marker.js
+++ src/components/services/marker.js
@@ -8,11 +8,14 @@ class Marker extends Component {
     super(props)
 
     this.state = {
-      infoShown: false
+      infoShown: false,
+      clickedOn: false
     }
 
     this.showInfo = this.showInfo.bind(this)
     this.hideInfo = this.hideInfo.bind(this)
+    this.markerClick = this.markerClick.bind(this)
+    this.markerRemove = this.markerRemove.bind(this)
   }
 
   showInfo () {
@@ -20,22 +23,53 @@ class Marker extends Component {
   }
 
   hideInfo () {
-    this.setState({ infoShown: false })
+    if (!this.state.clickedOn) {
+      this.setState({ infoShown: false })
+    }
+  }
+
+  markerClick () {
+    if (this.state.clickedOn) {
+      // act as a toggle
+      this.setState({
+        infoShown: false,
+        clickedOn: false
+      })
+    } else {
+      this.setState({
+        infoShown: true,
+        clickedOn: true
+      })
+    }
+  }
+
+  markerRemove (event) {
+    event.preventDefault()
+    this.setState({
+      infoShown: false,
+      clickedOn: false
+    })
   }
 
   render () {
+    const { infoShown, clickedOn } = this.state
     const { title, id, lat, lng, showList } = this.props
     const markerClasses = classNames(
       'marker-box',
-      { 'is-active': this.state.infoShown }
+      { 'is-active': infoShown }
+    )
+    const closeClasses = classNames(
+      'marker-info',
+      { 'show-close': clickedOn }
     )
 
     return (
       <div className={markerClasses}>
         <div className='marker-box-hover-wrapper' onMouseOver={this.showInfo} onMouseOut={this.hideInfo}>
-          <div className='marker'></div>
-          <div className='marker-info'>
+          <div className='marker' onClick={this.markerClick}></div>
+          <div className={closeClasses}>
             <h5>{title}</h5>
+            <a className='close-button' href='#' onClick={this.markerRemove}><span className='visuallyhidden'>Close</span></a>
             <p>
               <a href={'#' + id} onClick={showList}>Find out more</a>
               <br />
